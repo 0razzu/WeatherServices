b13433b36d4d3cc2fecac3755e0beb1eddab5d1d
0razzu <0razzu@protonmail.com>
2020-09-15 16:43:30 +0600

    ConsoleWeatherWidget.condition

--- src/main/java/widget/ConsoleWeatherWidget.java
+++ src/main/java/widget/ConsoleWeatherWidget.java
@@ -2,8 +2,21 @@ package widget;
 
 
 public class ConsoleWeatherWidget extends WeatherWidget {
+    private String condition;
+    
+    
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+    
+    
+    public String getCondition() {
+        return condition;
+    }
+    
+    
     @Override
     public String toString() {
-        return getCity() + ' ' + getCurrentTemperature() + " ºC";
+        return getCity() + ' ' + getCurrentTemperature() + " ºC" + ' ' + condition;
     }
 }
